5ceb722e8c25e9de098811d9dc26da6cff01c5d1
Mert Bayça <103841104+mertbayca@users.noreply.github.com>
2024-06-19 17:55:14 +0300

    Rework on services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -12,113 +12,91 @@ import {
 const Services = () => {
   return (
     <Section>
+      <Gradient />
       <div className="container">
         <Heading
           title="TRANSFORMIERE DEINE DIGITALE VERMARKTUNG"
           text="Das Internet hat eine globale Reichweite, was es Unternehmen ermöglicht, potenziell Milliarden von Menschen auf der ganzen Welt zu erreichen"
         />
-
-        <div className="relative">
-          <div className="relative z-1 flex items-center h-[39rem] mb-5 p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:p-20 xl:h-[46rem]">
-            <div className="absolute top-0 left-0 w-full h-full pointer-events-none md:w-3/5 xl:w-auto">
-              <img
-                className="w-full h-full object-cover md:object-right"
-                width={800}
-                alt="Playmakers"
-                height={730}
-                src={service1}
-              />
+        <Heading
+          title="4 Schritte zum Projektstart"
+        />
+          <div className="container mx-auto px-4 pb-40">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+              <div className="relative flex items-center">
+                <div className="bg-n-14 w-full h-20 rounded-full absolute left-0 top-0 transform translate-x-2 translate-y-2"></div>
+                <div className="bg-n-14 w-20 h-20 rounded-full flex items-center justify-center text-2xl translate-x-2 translate-y-2 font-bold text-white relative z-10">01</div>
+                <div className="bg-white w-full h-20 rounded-full flex items-center justify-between text-black px-4 relative z-10 ml-4">
+                  <p>Unverbindliches kennenlernen</p>
+                  <div className="bg-n-14 w-8 h-8 rounded-full"></div>
+                </div>
+              </div>
+              <div className="relative flex items-center">
+                <div className="bg-n-14 w-full h-20 rounded-full absolute left-0 top-0 transform translate-x-2 translate-y-2"></div>
+                <div className="bg-n-14 w-20 h-20 rounded-full flex items-center justify-center text-2xl translate-x-2 translate-y-2 font-bold text-white relative z-10">02</div>
+                <div className="bg-white w-full h-20 rounded-full flex items-center justify-between text-black px-4 relative z-10 ml-4">
+                  <p>Analyse zur Umsetzung der Ziele</p>
+                  <div className="bg-n-14 w-8 h-8 rounded-full"></div>
+                </div>
+              </div>
+              <div className="relative flex items-center">
+                <div className="bg-n-14 w-full h-20 rounded-full absolute left-0 top-0 transform translate-x-2 translate-y-2"></div>
+                <div className="bg-n-14 w-20 h-20 rounded-full flex items-center justify-center text-2xl translate-x-2 translate-y-2 font-bold text-white relative z-10">03</div>
+                <div className="bg-white w-full h-20 rounded-full flex items-center justify-between text-black px-4 relative z-10 ml-4">
+                  <p>Detailliertes Angebot</p>
+                  <div className="bg-n-14 w-8 h-8 rounded-full"></div>
+                </div>
+              </div>
+              <div className="relative flex items-center">
+                <div className="bg-n-14 w-full h-20 rounded-full absolute left-0 top-0 transform translate-x-2 translate-y-2"></div>
+                <div className="bg-n-14 w-20 h-20 rounded-full flex items-center justify-center translate-x-2 translate-y-2 text-2xl font-bold text-white relative z-10">04</div>
+                <div className="bg-white w-full h-20 rounded-full flex items-center justify-between text-black px-4 relative z-10 ml-4">
+                  <p>Kick Off des Projekts</p>
+                  <div className="bg-n-14 w-8 h-8 rounded-full"></div>
+                </div>
+              </div>
             </div>
-
-            <div className="relative z-10 max-w-[17rem] ml-auto p-4 rounded-lg bg-black bg-opacity-80 sm:bg-black sm:bg-opacity-60 md:bg-transparent md:bg-opacity-0">
-              <h4 className="h4 mb-4">Lorem Ipsum</h4>
-              <p className="body-2 mb-[3rem] text-n-3">
-                Lorem impsum dolor sit amet.
-              </p>
-              <ul className="body-2">
-                {playmakersServices.map((item, index) => (
-                  <li
-                    key={index}
-                    className="flex items-start py-4 border-t border-n-6"
-                  >
-                    <img width={24} height={24} src={check} />
-                    <div className="ml-4">
-                      <p className="mb-1 font-semibold">{item.title}</p>
-                      <p className="text-sm text-gray-600">{item.description}</p>
-                    </div>
-                  </li>
-                ))}
-              </ul>
+          </div>
+          <Heading
+          title="UNSERE WERTE"
+        />
+          <div className="container mx-auto px-4 pb-20">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+              <div>
+                <h3 className="text-2xl font-bold text-n-14 mb-2">Globaler Blick:</h3>
+                <p>Berücksichtigung globaler Trends und Märkte, um internationale Kunden effektiv zu unterstützen.</p>
+              </div>
+              <div>
+                <h3 className="text-2xl font-bold text-n-14 mb-2">Innovation:</h3>
+                <p>Unsere Bereitschaft, ständig neue Ideen, Ansätze und Technologien zu nutzen, um innovative Lösungen zu finden.</p>
+              </div>
+              <div>
+                <h3 className="text-2xl font-bold text-n-14 mb-2">Kundenorientierung:</h3>
+                <p>Unser Fokus liegt darauf, den Kunden in den Mittelpunkt zu stellen und maßgeschneiderte Lösungen zu bieten.</p>
+              </div>
+              <div>
+                <h3 className="text-2xl font-bold text-n-14 mb-2">Performance:</h3>
+                <p>Stets Spitzenleistungen erbringen.</p>
+              </div>
+              <div>
+                <h3 className="text-2xl font-bold text-n-14 mb-2">Teamwork:</h3>
+                <p>Wir setzen immer auf Teamarbeit, auch bei anstrengenden Projekten.</p>
+              </div>
+              <div>
+                <h3 className="text-2xl font-bold text-n-14 mb-2">Kreativität:</h3>
+                <p>Digitales Branding erfordert Kreativität, die wir gemeinsam mit unseren Kunden einbringen.</p>
+              </div>
             </div>
-
           </div>
 
-          <div className="relative z-1 grid gap-5 lg:grid-cols-2">
-            <div className="relative min-h-[39rem] border border-n-1/10 rounded-3xl overflow-hidden">
-              <div className="absolute inset-0">
-                <img
-                  src={service2}
-                  className="h-full w-full object-cover"
-                  width={630}
-                  height={750}
-                  alt="playmakers"
-                />
-              </div>
-
-              <div className="absolute inset-0 flex flex-col justify-end p-8 bg-gradient-to-b from-n-8/0 to-n-8/90 lg:p-15">
-                <h4 className="h4 mb-4">UNSERE WERTE</h4>
-                <p className="body-2 mb-[3rem] text-n-3">
-                  Inspirierendes Leadership, Zusammenarbeit, Globaler Blick, Kreativität, Innovation, Kundenorientierung
-                </p>
-              </div>
-
-              <PhotoChatMessage />
-            </div>
-
-            <div className="p-4 bg-n-7 rounded-3xl overflow-hidden lg:min-h-[46rem] lg:flex">
-              <div className="py-12 px-4 xl:px-8 lg:w-1/2">
-                <h4 className="text-xl font-bold mb-4" style={{ color: '#FF4343' }}>Globaler Blick:</h4>
-                <p className="text-base mb-6 text-n-3">
-                  Berücksichtigung globaler Trends und Märkte, um internationale Kunden effektiv zu unterstützen und grenzüberschreitende Chancen zu nutzen.
-                </p>
-                <h4 className="text-xl font-bold mb-4" style={{ color: '#FF4343' }}>Inspirierendes Leadership:</h4>
-                <p className="text-base mb-6 text-n-3">
-                  Förderung von inspirierendem Leadership, das Mitarbeiter dazu ermutigt, ihr volles Potenzial auszuschöpfen.
-                </p>
-                <h4 className="text-xl font-bold mb-4" style={{ color: '#FF4343' }}>Zusammenarbeit:</h4>
-                <p className="text-base mb-6 text-n-3">
-                  Both the designer’s and developer’s needs are important to Specify. Creating autonomously but working collaboratively.
-                </p>
-              </div>
-
-              <div className="py-12 px-4 xl:px-8 lg:w-1/2">
-                <h4 className="text-xl font-bold mb-4" style={{ color: '#FF4343' }}>Innovation:</h4>
-                <p className="text-base mb-6 text-n-3">
-                  Die Bereitschaft, kontinuierlich neue Ideen, Ansätze und Technologien zu erforschen und einzusetzen, um innovative Lösungen zu finden.
-                </p>
-                <h4 className="text-xl font-bold mb-4" style={{ color: '#FF4343' }}>Kundenorientierung:</h4>
-                <p className="text-base mb-6 text-n-3">
-                  Ein Fokus darauf, den Kunden in den Mittelpunkt aller Bemühungen zu stellen und ihre Bedürfnisse und Ziele zu verstehen, um maßgeschneiderte Lösungen anzubieten.
-                </p>
-                <h4 className="text-xl font-bold mb-4" style={{ color: '#FF4343' }}>Kreativität:</h4>
-                <p className="text-base text-n-3">
-                  Take advantage of our open source parsers to transform your design tokens and assets for all your platforms. You have full control over your design data.
-                </p>
-              </div>
-            </div>
-
-          </div>
-
-          <Gradient />
         </div>
         <div className="flex justify-center items-center mt-4">
           <Button href="/pricing" white>
           KONTAKTIERE UNS HIER
           </Button>
         </div>
-      </div>
       <BottomLine />
-    </Section>
+    </ Section>
   );
 };
 
